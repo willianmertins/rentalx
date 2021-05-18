5207897347e733e017dc9f07f49e874f6f86c2d2
Willian Mertins <74335481+willianmertins@users.noreply.github.com>
2021-05-17 21:50:24 -0300

    chore: upload e stream de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,14 @@ import { Response, Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
+
+import multer from "multer";
+
+
+const upload = multer({
+  dest: "./tmp", 
+});
 
 const categoriesRoutes = Router();
 // const categoriesRepository = new CategoriesRepository();
@@ -12,6 +20,11 @@ categoriesRoutes.post("/", (request,response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request,response);
-})
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request,response);
+});
+
 
 export { categoriesRoutes };
