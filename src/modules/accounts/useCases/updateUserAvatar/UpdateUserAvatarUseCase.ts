b10cb4af3e8d1a134bbd068b273f55eff4b15f3d
Willian Mertins <74335481+willianmertins@users.noreply.github.com>
2021-06-01 20:28:19 -0300

    Chore: upload avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { deleteFile } from "../../../../utils/file";
 
 interface IRequest{
   user_id: string;
@@ -17,6 +18,9 @@ class UpdateUserAvatarUseCase {
   async execute({ user_id, avatar_file }: IRequest): Promise<void>{
     const user = await this.usersRepository.findById(user_id);
 
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }    
     user.avatar = avatar_file;
 
     await this.usersRepository.create(user);
