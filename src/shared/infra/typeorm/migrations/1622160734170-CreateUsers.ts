2f35a34df940ae2a0538649e446acd5212a6ee44
willianmertins <willian.mertins@gmail.com>
2021-11-03 21:51:42 -0300

    commit geral

--- src/shared/infra/typeorm/migrations/1622160734170-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1622160734170-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1622160734170 implements MigrationInterface {
                 columns: [
                     {
                         name:"id",
-                        type:"uuid"
+                        type:"uuid",
+                        isPrimary: true,
                     },
                     {
                         name:"name",
